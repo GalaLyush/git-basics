88abdd75232a4872b375ec0152d5c01d4ff05346
Galalyush <galalyush@gmail.com>
2021-08-24 12:19:44 +0300

    first commit

---
+++ base.py
@@ -0,0 +1,14 @@
+a = int(input('Введите число: '))
+b = int(input('Введите число: '))
+c = input('Знак операции: ')
+
+if c == '+':
+    print(a + b)
+elif c == '-':
+    print(a - b)
+elif c == '*':
+    print(a * b)
+else:
+    print(a / b)
+print('Hi')
+print()
